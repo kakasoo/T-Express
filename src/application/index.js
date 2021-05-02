25b259af21213e6c991b42df28ceb90f9664db23
kakasoo <kscodebase@gmail.com>
2021-05-02 16:01:17 +0900

    [feat] 이벤트 함수의 실행을 담당할 로직을 작성 ( handlers )

--- src/application/index.js
+++ src/application/index.js
@@ -5,9 +5,8 @@ import { flat, slice } from "../util";
 class Route {
     constructor(path) {
         this.path = path;
-        this.stack = [];
+        this.stack = []; // path 단위로 route 된 이후의 stack이므로 메서드 분기를 의미
         this.methods = {};
-
         METHODS.forEach((METHOD) => {
             const method = METHOD.toLowerCase();
             this[method] = (...handles) => {
@@ -18,22 +17,67 @@ class Route {
                         throw new Error("Route's handle error!");
                     }
 
+                    // 진입한 이후이므로 path는 '/'가 되야 마땅하다.
                     const layer = new Layer("/", handle, {});
                     layer.method = method;
-
                     this.methods[method] = true;
                     this.stack.push(layer);
                 }
+                return this;
             };
         });
+    }
+
+    // 실질적으로 request와 response 사이의 처리를 담당한다.
+    dispatch(req, res, done) {
+        let index = 0;
+        // 어떠한 메서드도 아직 생성되지 않은 경우
+        if (this.stack.length === 0) {
+            return done();
+        }
+
+        const method = req.method.toLowerCase();
+        if (method === "head" && !this.method["head"]) {
+            method = "get";
+        }
+
+        req.route = this;
+
+        const next = (err) => {
+            if (err && err === "route") {
+                return done(err);
+            }
+
+            if (err && err === "router") {
+                return done(err);
+            }
+
+            // 더 이상 진입할 layer가 없는 경우 ( method가 없는 경우 )
+            const layer = stack[index++];
+            if (!layer) {
+                return done(err);
+            }
+
+            // 필요로 하는 메서드와 현재 layer의 메서드가 다른 경우
+            if (layer.method && layer.method !== method) {
+                return next(err);
+            }
+
+            if (err) {
+                layer.handleError(err, req, res, next);
+            } else {
+                layer.handleRequest(req, res, next);
+            }
+        };
+        next();
     }
 }
 
 class Layer {
-    constructor(path, route, option) {
+    constructor(path, fn, options) {
         this.path = path;
         this.methods = {};
-        this.route = route;
+        this.handle = fn;
     }
 
     handleMethod(methodName) {
@@ -45,6 +89,36 @@ class Layer {
             methodName = "get";
         }
         return Boolean(this.methods[methodName]);
+    }
+
+    handleError(error, req, res, next) {
+        const fn = this.handle;
+
+        // function's length is parameter number;
+        if (fn.length !== 4) {
+            // not a standard request handler
+            return next(error);
+        }
+
+        try {
+            fn(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    handleRequest() {
+        const fn = this.handle;
+
+        if (fn.length > 3) {
+            return next();
+        }
+
+        try {
+            fn(req, res, next);
+        } catch (err) {
+            next(err);
+        }
     }
 }
 
