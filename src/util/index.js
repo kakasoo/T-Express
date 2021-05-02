47ff02069e2be39ee5b4f7bd04d761d3afd9ab8f
kakasoo <kscodebase@gmail.com>
2021-05-02 17:49:24 +0900

    [chore] 테스트를 위한 콘솔 함수 작성

--- src/util/index.js
+++ src/util/index.js
@@ -1,2 +1,10 @@
 export const slice = Array.prototype.slice;
 export const flat = Array.prototype.flat;
+
+let MESSAGE_SEQ = 1;
+export const printMessage = (data) => {
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    console.log("ID : ", MESSAGE_SEQ++);
+    console.log(data);
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+};
